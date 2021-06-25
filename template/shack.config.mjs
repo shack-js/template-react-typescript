a32f63133e96c900511682aede7a84728a37147f
josh <postor@gmail.com>
2021-06-25 18:10:23 +0800

    basic

--- template/shack.config.mjs
+++ template/shack.config.mjs
@@ -1,17 +1,45 @@
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
 
 export default {
   entry: './web/index.tsx',
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        options: {
-          compilerOptions: {
-            module: "ESNext",
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      }, {
+        test: /\.(png|jpe?g|gif|ttf|woff2?|eot|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      }, {
+        test: /\.(m|c)?(t|j)sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-typescript',
+              '@babel/preset-react',
+              '@babel/preset-env',
+            ],
+            plugins: [
+              "@babel/plugin-transform-runtime",
+              [
+                "auto-import", {
+                  "declarations": [
+                    { "default": "React", "path": "react" }
+                  ]
+                }
+              ],
+            ]
           }
         }
       }
@@ -20,17 +48,27 @@ export default {
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".mjs", ".cjs"]
   },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'my-react',
-    template: join(dirname(fileURLToPath(import.meta.url)), 'web', 'index.html'),
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'my-react',
+      template: join(dirname(fileURLToPath(import.meta.url)), 'web', 'index.html'),
+    }),
+    new MiniCssExtractPlugin(),
+    // new BundleAnalyzerPlugin(),
+    new CleanWebpackPlugin(),
+  ],
   output: {
     path: join(dirname(fileURLToPath(import.meta.url)), 'dist', 'web'),
-    filename: '[contenthash].js'
+    filename: '[name].[contenthash].js',
+    publicPath: '/'
   },
   optimization: {
     splitChunks: {
       chunks: 'all',
     },
   },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+  }
 }
